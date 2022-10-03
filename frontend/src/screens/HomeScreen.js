caf1cc935770a2fe0b8415967f07f8efa033c59e
Shanks08 <sakagami008@gmail.com>
2022-10-03 23:17:30 +0530

    Completed Product Page

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -3,6 +3,7 @@ import axios from 'axios';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Product from '../component/Product';
+import { Helmet } from 'react-helmet-async';
 
 const reducer = (state, action) => {
 	switch (action.type) {
@@ -39,6 +40,9 @@ export default function HomeScreen() {
 	}, []);
 	return (
 		<div>
+			<Helmet>
+				<title>famicom</title>
+			</Helmet>
 			<h1>Featured Products</h1>
 			<div className="products">
 				{loading ? (
